45a42308c1a448aa764bf793bb0a9b41cfedc3cf
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-04-28 05:42:10 +0700

    update 01_list.c

--- 01_list.c
+++ 01_list.c
@@ -7,12 +7,38 @@ typedef struct	node
 	struct node	*next;
 }	Node;
 
+void	print_list(Node	*head);
+
 int	main()
 {
 	Node	a;
-	a.value = 5;
+	Node	b;
+	Node	c;
 
-	printf("Node a.value: %d\n", a.value);
+	a.value = 5;
+	b.value = 6;
+	c.value = 7;
+	a.next = &b;
+	b.next = &c;
+	c.next = NULL;
+
+	print_list( &a );
 
 	return (0);
+}
+
+void	print_list(Node	*head)
+{
+	Node	*current;
+	int		i;
+
+	current = head;
+	i = 0;
+
+	while (current != NULL)
+	{
+		printf("Node %d: %d\n", i, current->value);
+		i++;
+		current = current->next;
+	}
 }
